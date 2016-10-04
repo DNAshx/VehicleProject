c1705b890e0b0359af6007f401c98bffef6ea434
Alexey Polovinka <alexey.polovinka@diatomenterprises.com>
2016-10-04 10:49:07 +0300

    Vehicle class + new Common project

---
+++ Src/VehicleSolution/001_GlauxSoft.Vehicle.Common/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlauxSoft.Vehicle.Common
+{
+    public class Vehicle : GlauxSoft.GreenTransport.Repository.Vehicle
+    { 
+        public List<string> VehicleType
+        {
+            get;
+            set;
+        }
+    }
+}
